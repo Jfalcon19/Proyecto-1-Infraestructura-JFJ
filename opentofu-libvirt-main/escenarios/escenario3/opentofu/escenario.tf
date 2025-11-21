536e4d3021720d1931d5b1b1e66c8a684f823732
Jfalcon19 <jesusfaljim@gmail.com>
2025-11-21 13:29:28 +0100

    Soluciono problemas de nombres repetidos con otras máquinas que tengo, y el escenario de opentofu funciona

--- opentofu-libvirt-main/escenarios/escenario3/opentofu/escenario.tf
+++ opentofu-libvirt-main/escenarios/escenario3/opentofu/escenario.tf
@@ -9,29 +9,29 @@ locals {
   ##############################################
 
   networks = {
-    red-externa = {
-      name      = "red-externa"
+    red-externa-pry1 = {
+      name      = "red-externa-pry1"
       mode      = "nat"
       domain    = "example.com"
       addresses = ["192.168.200.0/24"]
-      bridge    = "br-ex"
+      bridge    = "br-ex-pry1"
       dhcp      = true
       dns       = true
       autostart = true
     }
 
-    red-conf = {
-      name      = "red-conf"
+    red-conf-pry1 = {
+      name      = "red-conf-pry1"
       mode      = "none" # sin conectividad
       addresses = ["192.168.201.0/24"]
-      bridge    = "br-conf"
+      bridge    = "br-conf-pry1"
       autostart = true
     }
 
-    red-datos = {
-      name      = "red-datos"
+    red-datos-pry1 = {
+      name      = "red-datos-pry1"
       mode      = "none" # sin conectividad
-      bridge    = "br-datos"
+      bridge    = "br-datos-pry1"
       autostart = true
     }
   }
@@ -42,15 +42,15 @@ locals {
 
   servers = {
     apache2 = {
-      name       = "apache2"
+      name       = "apache2-pry1"
       memory     = 1024
       vcpu       = 1
       base_image = "debian13-base.qcow2"
 
       networks = [
-        { network_name = "red-externa", wait_for_lease = true },
-        { network_name = "red-conf" },
-        { network_name = "red-datos" }
+        { network_name = "red-externa-pry1", wait_for_lease = true },
+        { network_name = "red-conf-pry1" },
+        { network_name = "red-datos-pry1" }
       ]
 
       user_data      = "${path.module}/cloud-init/server1/user-data.yaml"
@@ -58,15 +58,15 @@ locals {
     }
 
     mariadb = {
-      name       = "mariadb"
+      name       = "mariadb-pry1"
       memory     = 1024
       vcpu       = 1
       base_image = "ubuntu2404-base.qcow2"
 
       networks = [
-        { network_name = "red-externa", wait_for_lease = true },
-        { network_name = "red-conf" },
-        { network_name = "red-datos" }
+        { network_name = "red-externa-pry1", wait_for_lease = true },
+        { network_name = "red-conf-pry1" },
+        { network_name = "red-datos-pry1" }
       ]
 
       user_data      = "${path.module}/cloud-init/server2/user-data.yaml"
